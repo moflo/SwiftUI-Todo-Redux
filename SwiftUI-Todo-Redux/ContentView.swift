f1add1559fa42654dc4bac44b7183557acf5e3bb
admin <admin@demo.com>
2019-06-22 10:31:10 -0700

    initial build

---
+++ SwiftUI-Todo-Redux/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Todo-Redux
+//
+//  Created by admin on 6/22/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("SwiftUI Todo App With Redux State")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
