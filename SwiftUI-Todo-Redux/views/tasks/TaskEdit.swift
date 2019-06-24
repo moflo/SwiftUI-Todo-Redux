03140e33e72c9e93237909785bfad8d1fb2f812d
mobileflowllc <fieldforceapp@gmail.com>
2019-06-24 11:59:44 -0700

    adding static form

--- SwiftUI-Todo-Redux/views/tasks/TaskEdit.swift
+++ SwiftUI-Todo-Redux/views/tasks/TaskEdit.swift
@@ -8,33 +8,75 @@
 
 import SwiftUI
 
+struct FieldSetText: View {
+    var label: String
+    var placeHolder: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.headline)
+            TextField(.constant(""), placeholder: Text(placeHolder))
+                .padding(.all)
+                .background(Color("form-field-background"), cornerRadius: 5.0)
+        }
+        .padding(.horizontal, 15)
+    }
+}
+
+struct RoundedButton: View {
+    var body: some View {
+        Button(action: {}) {
+            HStack {
+                Spacer()
+                Text("Save")
+                    .font(.headline)
+                    .color(Color.white)
+                Spacer()
+            }
+        }
+        .padding(.vertical, 10.0)
+        .background(Color.green, cornerRadius: 8.0)
+        .padding(.horizontal, 40)
+    }
+}
+
 struct TaskEdit: View {
     @EnvironmentObject var store: AppState
     let task: Task
-    
+
     var body: some View {
-        VStack {
-            Text("TaskDetail")
-            AnyView(
-                Text(task.title).font(.title))
-            AnyView(
-                Text(task.id)
-            )
-            if task.isDone {
-                Text("Done")
-            } else {
-                Text("Not Done")
+        NavigationView {
+            List {
+                VStack(alignment: .leading) {
+                    FieldSetText(label: "TITLE", placeHolder: "Task title")
+                    FieldSetText(label: "DESCRIPTION", placeHolder: "Task description")
+
+                    RoundedButton().padding(.top, 20)
+                }
+                .padding(.top, 20)
+                .listRowInsets(EdgeInsets())
             }
+
+            .navigationBarTitle(Text("New Restaurant"))
+            .navigationBarItems(leading:
+                Button(action: {}, label: {
+                    Text("Cancel")
+            }))
+            .navigationBarItems(trailing:
+                Button(action: {}, label: {
+                    Text("Save")
+            }))
         }
     }
 }
 
 #if DEBUG
-struct TasksEdit_Previews: PreviewProvider {
-    static var previews: some View {
-        TaskEdit(task: sampleStore.tasksState.tasks[0])
-            .environmentObject(sampleStore)
-            .previewLayout(.fixed(width: 375, height: 1000))
+    struct TasksEdit_Previews: PreviewProvider {
+        static var previews: some View {
+            TaskEdit(task: sampleStore.tasksState.tasks[0])
+                .environmentObject(sampleStore)
+                .previewLayout(.fixed(width: 375, height: 1000))
+        }
     }
-}
 #endif
