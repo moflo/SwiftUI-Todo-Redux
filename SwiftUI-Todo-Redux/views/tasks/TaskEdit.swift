f9f1f993539495f67eef6c0ca5a5daf5979fff50
admin <admin@demo.com>
2019-06-24 11:24:48 -0700

    Task edit

---
+++ SwiftUI-Todo-Redux/views/tasks/TaskEdit.swift
@@ -0,0 +1,38 @@
+//
+//  TaskEdit
+//  SwiftUI-Todo-Redux
+//
+//  Created by moflo on 6/22/19.
+//  Copyright © 2019 Mobile Flow LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskEdit: View {
+    @EnvironmentObject var store: AppState
+    let task: Task
+    
+    var body: some View {
+        VStack {
+            Text("TaskDetail")
+            AnyView(
+                Text(task.title).font(.title))
+            AnyView(
+                Text(task.id)
+            )
+            if task.isDone {
+                Text("Done")
+            } else {
+                Text("Not Done")
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct TasksEdit_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskEdit(task: sampleStore.tasksState.tasks[0]).environmentObject(sampleStore)
+    }
+}
+#endif
