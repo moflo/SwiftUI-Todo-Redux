0812767397e335b56357ae7d965bbd1fc5b2b004
admin <admin@demo.com>
2019-06-24 18:31:46 -0700

    edit task options

--- SwiftUI-Todo-Redux/views/tasks/TaskDetail.swift
+++ SwiftUI-Todo-Redux/views/tasks/TaskDetail.swift
@@ -31,12 +31,42 @@ struct TaskDetail: View {
         }
     }
 
+    var TaskEdit: some View {
+        Form {
+            Section(header: Text("Task Information")) {
+                VStack(alignment: .leading) {
+                    FieldSetText(label: "TITLE", placeHolder: "Task title")
+                    FieldSetText(label: "DESCRIPTION", placeHolder: "Task description")
+
+                    FieldSetText(label: "DUMMY", placeHolder: "dummy placeholder")
+                    FieldSetText(label: "DUMMY", placeHolder: "dummy placeholder")
+                    FieldSetText(label: "DUMMY", placeHolder: "dummy placeholder")
+                    FieldSetText(label: "DUMMY", placeHolder: "dummy placeholder")
+                    FieldSetText(label: "DUMMY", placeHolder: "dummy placeholder")
+                }
+                .padding(.vertical, 20)
+                .listRowInsets(EdgeInsets())
+            }
+            Section(header: Text("Task Owner")) {
+                VStack(alignment: .leading) {
+                    FieldSetText(label: "OWNER", placeHolder: "Task owner")
+                }
+                .padding(.vertical, 20)
+                .listRowInsets(EdgeInsets())
+            }
+
+            RoundedButton().padding(.vertical, 20)
+        }
+    }
+
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
                 if self.mode?.value == .active {
                     Button(action: {
                         self.mode?.animation().value = .inactive
+                        // Update current taskID
+                        // store.dispatch(aciton: TaskActions.updateTask(id: task.id, task: draftTask))
                     }) {
                         Text("Save")
                     }
@@ -49,7 +79,7 @@ struct TaskDetail: View {
             if self.mode?.value == .inactive {
                 TaskSummary
             } else {
-                TaskEdit(task: $draftTask).environmentObject(store)
+                TaskEdit
             }
         }
         .padding()
