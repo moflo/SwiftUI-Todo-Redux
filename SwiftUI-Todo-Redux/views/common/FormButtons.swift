3aea78c705c59235bf31f9f207da9adc175e00ea
mobileflowllc <fieldforceapp@gmail.com>
2019-06-24 22:03:07 -0700

    form button update

---
+++ SwiftUI-Todo-Redux/views/common/FormButtons.swift
@@ -0,0 +1,45 @@
+//
+//  FormButtons
+//  SwiftUI-Todo-Redux
+//
+//  Created by moflo on 6/22/19.
+//  Copyright © 2019 Mobile Flow LLC. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+// Class to respond to editText commit, change responder / focus
+
+struct FieldSetText: View {
+    var label: String
+    var placeHolder: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.headline)
+            TextField(.constant(""), placeholder: Text(placeHolder))
+                .padding(.all)
+                .background(Color("form-field-background"), cornerRadius: 5.0)
+        }
+        .padding(.horizontal, 15)
+    }
+}
+
+struct RoundedButton: View {
+    var body: some View {
+        Button(action: {}) {
+            HStack {
+                Spacer()
+                Text("Save")
+                    .font(.headline)
+                    .color(Color.white)
+                Spacer()
+            }
+        }
+        .padding(.vertical, 10.0)
+        .background(Color.green, cornerRadius: 8.0)
+        .padding(.horizontal, 40)
+    }
+}
---
+++ SwiftUI-Todo-Redux/views/common/KeyboardObserver.swift
@@ -0,0 +1,76 @@
+//
+//  KeyboardObserver
+//  SwiftUI-Todo-Redux
+//
+//  Created by moflo on 6/22/19.
+//  Copyright © 2019 Mobile Flow LLC. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+// Class to respond to keyboard events
+
+final class KeyboardObserver: BindableObject {
+    let didChange = PassthroughSubject<Void, Never>()
+
+    public var rects: [CGRect]
+    public var keyboardRect: CGRect = CGRect()
+
+    // keyboardWillShow notification may be posted repeatedly,
+    // this flag makes sure we only act once per keyboard appearance
+    public var keyboardIsHidden = true
+
+    public var slide: Length = 0 {
+        didSet {
+            didChange.send()
+        }
+    }
+
+    public var showField: Int = 0 {
+        didSet {
+            updateSlide()
+        }
+    }
+
+    init(_ textFieldCount: Int = 0) {
+        rects = [CGRect](repeating: CGRect(), count: textFieldCount)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardDidHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc func keyBoardWillShow(notification: Notification) {
+        if keyboardIsHidden {
+            keyboardIsHidden = false
+            if let rect = notification.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect {
+                keyboardRect = rect
+                updateSlide()
+            }
+        }
+    }
+
+    @objc func keyBoardDidHide(notification _: Notification) {
+        keyboardIsHidden = true
+        updateSlide()
+    }
+
+    func updateSlide() {
+        if keyboardIsHidden {
+            slide = 0
+        } else {
+            let tfRect = rects[self.showField]
+            let diff = keyboardRect.minY - tfRect.maxY
+
+            if diff > 0 {
+                slide += diff
+            } else {
+                slide += min(diff, 0)
+            }
+        }
+    }
+}
